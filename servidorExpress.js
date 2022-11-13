6a02acd349f32c1f290365377289c01f7e81186c
Mariano Lopez <lopezmariano3108@gmail.com>
2022-11-12 21:26:27 -0300

    Termine el desafio de express

--- servidorExpress.js
+++ servidorExpress.js
@@ -1,53 +1,84 @@
 const express = require('express');
-const fs = require('fs')
+const { Router } = express
+const router = new Router()
 
 const PORT = 8080;
 const app = express()
 
-//CLASE + ASIGNACION DE LA DATA A UNA VARIABLE
+// MIDDLEWARE
 
-class Contenedor {
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-    constructor(nombre) {
-        this.nombre = nombre;
-    }
-
-    getAll() {
-        try {
-            const lectura = fs.readFileSync(this.nombre, "utf-8")
-            return lectura
-        }
-        catch (error) {
-            throw new Error("Error en la lectura")
-        }
-    }
-}
-
-const contenedor1 = new Contenedor('./productos.json')
-
-let data = contenedor1.getAll()
-
-//LOGICA DE EXPRESS Y FS
-
-app.get('/', (req, res) => {
-    res.send('<h1 style="color: blue;">Bienvenidos al servidor express</h1>'
-    )
-})
-
-app.get('/productos', (req, res) => {
-    res.send(`Array de productos disponibles: <br><br> ${JSON.stringify(data)}`)
-})
-
-data = JSON.parse(data)
-
-app.get('/productoRandom', (req, res) => {
-    res.send(`Productos random: <br><br> ${JSON.stringify(data[Math.floor(Math.random() * data.length)])}`)
-})
+//FORMULARIO EN "/"
+app.use(express.static("./public"))
 
 const server = app.listen(PORT, () => {
     console.log(`servidor expres desde el puerto: ${PORT}`);
 })
 
+//MANEJO DE ERRORES
 server.on("error", error => {
-    console.log(`Èrror en server ${error}`);
+    console.log(error);
 })
+
+//ARRAY PARA PERSISTENCIA DE MEMORIA
+const productos = []
+
+//CONFIG DE ROUTER/RUTA RAIZ
+app.use('/api', router)
+
+//RUTAS GET
+app.get("/", (req, res) => {
+    res.json("ruta raiz")
+})
+
+router.get("/productos", (req, res) => { //RUTA /API/PROUCTOS
+    res.json({ productos, productos })
+})
+
+router.get("/productos/:id", (req, res) => { //RUTA /API/PROUCTOS/:ID
+    const productoEncontrado = productos.find(i => parseInt(i.id) === parseInt(req.params.id))
+    if (!productoEncontrado) {
+        res.json({ error: "producto no encontrado" })
+    } else {
+        res.json(productoEncontrado)
+    }
+})
+
+//RUTAS CON METODO POST
+
+router.post("/productos", (req, res) => {
+    const newObj = req.body
+    productos.push({ ...newObj, id: productos.length + 1 })
+    res.json("recibida")
+})
+
+//RUTA CON METODO PUT
+
+router.put("/productos/:id", (req, res) => {
+    const { title, price, thumbnail } = req.body
+    const { id } = req.params
+    productos[parseInt(id - 1)].title = title
+    productos[parseInt(id - 1)].price = price
+    productos[parseInt(id - 1)].thumbnail = thumbnail
+    res.json("producto modificado")
+})
+
+//RUTA CON METODO DELETE SEGUN EL ID 
+
+router.delete("/productos/:id", (req, res) => {
+    const productoEncontrado = productos.find(i => parseInt(i.id) === parseInt(req.params.id))
+    if (!productoEncontrado) {
+        res.json({ error: "producto no encontrado" })
+    } else {
+        productos.splice(productos.indexOf(productoEncontrado), 1)
+        res.send("Producto eliminado")
+    }
+})
+
+
+
+
+
+
